b43ff11c09f1617c12d3b777213c3bbdbb329e14
Friedrich Weber <fred@reichbier.de>
2010-01-21 17:06:58 +0100

    updated compile.sh to define __OOC_USE_GC__.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ooc test.ooc -noclean
 cd ooc_tmp
-gcc -g -shared -fPIC -o../libtest.so -I. pyooc/test.c sdk/lang/*.c sdk/structs/*.c sdk/text/*.c
+gcc -D__OOC_USE_GC__ -g -shared -fPIC -o../libtest.so -I. pyooc/test.c sdk/lang/*.c sdk/structs/*.c sdk/text/*.c sdk/io/*.c
 cd ..
 #rm -rf ooc_tmp
