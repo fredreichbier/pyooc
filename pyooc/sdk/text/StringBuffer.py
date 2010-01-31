e2900a64449ee919889260470d31e9122a05ab3e
Friedrich Weber <fred@reichbier.de>
2010-01-31 02:05:38 +0100

    Did some sdk wrapping

---
+++ pyooc/sdk/text/StringBuffer.py
@@ -0,0 +1,18 @@
+from pyooc import Class
+
+def bind(lib):
+    module = lib.get_module('text/StringBuffer')
+
+    class StringBuffer(Class):
+        _methods_ = [
+                ('write_chr', None, [lib.types.Char]),
+                ('toString', lib.types.String, None),
+        ]
+        _constructors_ = [
+                ('', None),
+        ]
+
+    StringBuffer.bind(module)
+
+    module.StringBuffer = StringBuffer
+    return module
